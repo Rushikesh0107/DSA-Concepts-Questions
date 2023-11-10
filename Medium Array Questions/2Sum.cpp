d3a654e5b9fb054b962f4424cf8c39aaa42d1140
Rushikesh0107 <rushikeshdadas03@gmail.com>
2023-11-10 10:55:40 +0530

    dutch national flag sorting

--- Medium Array Questions/2Sum.cpp
+++ Medium Array Questions/2Sum.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main(){
+
+    //solved using hash map
     
     int nums[] = {2,7,11,15};
     int target = 9;
@@ -18,7 +20,30 @@ int main(){
         m[a] = i;
     }
 
-    
+    //here the time complexity is O(nlogn) for ordered map 
+    //And O(n) for unordered map in best and average case in worst case it is O(n2)
+    //space complexity is O(n) for both
+
+
+    //solved using two piointer approach
+
+    int i = 0, j = n -1;
+
+    while(i < j){
+        if(nums[i] + nums[j] == target){
+            cout<<"There is a pair"<<endl;  
+        }
+
+        if(nums[i] + nums[j] > target){
+            j--;
+        }
+        else{
+            i++;
+        }
+    }
+
+    //here the time complexity is O(n) + O(nlogn)
+    //space complexity is O(1)
 
     return 0;
 }
