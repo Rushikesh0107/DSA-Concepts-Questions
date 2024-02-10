e03a71f7382806768de8c709d930789a171c67fb
Rushikesh0107 <rushikeshdadas03@gmail.com>
2024-02-10 19:48:17 +0530

    Add functions to delete head and tail of DLL

--- Linked-List/DLL.cpp
+++ Linked-List/DLL.cpp
@@ -39,12 +39,41 @@ Node* ConvertArrayToDLL(vector<int> arr){
     return head;
 }
 
+//deleting the head of the DLL
+
+Node* DeleteHead(Node* head){
+    if(head == NULL) return head;
+    
+    Node* prev = head;
+    head = head->next;
+    head->back = nullptr;
+    prev->next = nullptr;
+    free(prev);
+}
+
+//deleting the tail of the DLL
+
+Node* DeleteTail(Node* head){
+    if(head == NULL || head->next == NULL) return delete head;
+    
+    Node* temp = head;
+    
+    while(temp->next->next != nullptr){
+        temp = temp->next;
+    }
+    temp->next->back = nullptr;
+    temp->next = nullptr;
+    free(temp->next);
+    return head;
+}
+
 int main(){
     vector<int> arr = {1,2,3,4,5};
     
     Node* head = ConvertArrayToDLL(arr);
     
-    Node* temp = head;
+    Node* temp = DeleteHead(head);
+    DeleteTail(temp);
 
     //printing DLL in forward direction
     
